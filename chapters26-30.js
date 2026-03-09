f0fa9d732a496e6813712f10f27204fca316a39a
MaheenDanish09 <mariammaheen000@gmail.com>
2026-03-09 15:38:25 +0500

    JS chapters 21-40

--- chapters26-30.js
+++ chapters26-30.js
@@ -13,9 +13,31 @@
 // document.writeln("The absolute value of " + input + " is " + Math.abs(input));
 
 // QUESTION 4 //
-
+// document.writeln("The random dice value is: " + Math.floor(Math.random()*6));
 
 // QUESTION 5 //
+// var randomNum = Math.floor(Math.random()*3);
+// document.writeln(randomNum + "<br>");
+// if(randomNum === 1){
+// document.writeln("random coin value: " + "Heads");
+// }else if(randomNum === 2){
+//     document.writeln("random coin value: " + "Tails");
+// };
+
 // QUESTION 6 //
+// document.writeln("random number between 1 and 100: " + Math.floor(Math.random()*100));
+
 // QUESTION 7 //
+// let userInput = prompt("Enter your weight");
+// userInput = parseInt(userInput);
+// let unit = "kilograms";
+// document.writeln("The weight of user is: " + userInput + " "+ unit);
+
 // QUESTION 8 //
+// let secretNumber = Math.floor(Math.random()*10);
+// let userInput = prompt("Guess the secret number");
+// if(userInput === secretNumber){
+// document.writeln("Congratulations! You successfully guessed the secret number");
+// }else{
+//     document.writeln("Sorry! That is not the secret number.");
+// };
--- chapters31-34.js
+++ chapters31-34.js
@@ -1,15 +1,56 @@
 //////////DATE METHODS//////////
 
 // QUESTION 1 //
+// let date = new Date();
+// document.writeln(date);
+
 // QUESTION 2 //
+// let months = ["January" , "February" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"];
+// let currentMonth = new Date().getMonth();
+// document.writeln("Current Month: " + months[currentMonth]);
+
 // QUESTION 3 //
+// let days = ["Mon" , "Tue" , "Wed" , "Thu" , "Fri" , "Sat" , "Sun"];
+// let currentDay = new Date().getDay();
+// document.writeln("Today is " + days[currentDay]);
+
 // QUESTION 4 //
+// let currentDay = new Date().getDay();
+// if(currentDay === "0" || currentDay === "6"){
+// alert("It's Fun Day");
+// };
+
 // QUESTION 5 //
+// var date = new Date().getMonth();
+// if(date <= 16){
+    // document.writeln("First fifteen days of the month");
+// }else{
+    // document.writeln("Last fifteen days of the month");
+// };
+
 // QUESTION 6 //
+// document.writeln("Current Date: " + new Date() + "<br>" + "Elapsed milliseconds since January 1, 1970: " + new Date().getTime() + "<br>" + "Elapsed minutes since January 1, 1970: " + new Date().getTime()/60000);
+
 // QUESTION 7 //
+// let time = new Date().getHours();
+// if(time < 12){
+    // alert("It's AM");
+// }else{
+    // alert("It's PM");
+// };
+
 // QUESTION 8 //
+// let laterDate = new Date(2030, 11, 31);
+// document.writeln("Later Date: " + laterDate);
+
 // QUESTION 9 //
+
 // QUESTION 10 //
+// var referenceDate = new Date();
+// var startDate = new Date("January 1, 2026 00:00:00");
+// var difference = referenceDate - startDate;
+// document.writeln("On reference date " + referenceDate + ",<br>" + Math.floor(difference/1000) + " seconds have passed since beginning of 2026");
+
 // QUESTION 11 //
 // QUESTION 12 //
 // QUESTION 13 //
